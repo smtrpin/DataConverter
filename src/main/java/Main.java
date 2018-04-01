00f31c10309308f4b9cc5c0e39ad20069eeee14c
two cookie <rolxprod@mail.ru>
2018-04-01 18:16:27 +0300

    Added support for multithreading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,19 @@
 import com.twocookie.converter.Converter;
 import com.twocookie.converter.exception.DataConverterException;
+import com.twocookie.converter.generator.HTMLGenerator;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
 
   public static void main(String[] args) {
     try {
       Converter converter = new Converter(args);
-      String convertResult = converter.convert();
+      HashSet<HTMLGenerator> convertResult = converter.convert();
       System.out.println(convertResult);
-    } catch (DataConverterException | IOException e) {
+    } catch (DataConverterException | IOException | InterruptedException | ExecutionException e) {
       System.out.println(e.getMessage());
     }
   }
